9afac864d8cf35ac27b328be10bd5dc795520ff0
MarcusAfolabi <biormarcus@gmail.com>
2023-05-12 11:10:46 +0100

    user alert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,23 +88,160 @@
         const pageOneCharLimit = 160;
         const pageTwoCharLimit = 154;
 
+        // $(document).ready(function() {
+        //     // Prefixes and corresponding unit charges per page
+        //     const prefixCharges = {
+        //         "234705": 2.2,
+        //         "234805": 2.2,
+        //         "234807": 2.2,
+        //         "234815": 2.2,
+        //         "234811": 2.2,
+        //         "234905": 2.2,
+        //         "234803": 1.9,
+        //         "234706": 1.9,
+        //         "234703": 1.9,
+        //         "234810": 1.9,
+        //         "234806": 1.9,
+        //         "234813": 1.9,
+        //         "234816": 1.9,
+        //         "234814": 1.9,
+        //         "234903": 1.9,
+        //         "234906": 1.9,
+        //         "234708": 1.9,
+        //         "234802": 1.9,
+        //         "234808": 1.9,
+        //         "234812": 1.9,
+        //         "234701": 1.9,
+        //         "234902": 1.9,
+        //         "234907": 1.9,
+        //         "234809": 1.9,
+        //         "234817": 1.9,
+        //         "234818": 1.9,
+        //         "234909": 1.9,
+        //         "234908": 1.9,
+        //         "234709": 5,
+        //         "2347027": 5,
+        //         "2347028": 4,
+        //         "2347029": 4,
+        //         "234819": 4,
+        //         "2347026": 4,
+        //         "2347025": 4,
+        //         "234704": 4,
+        //         "234707": 4,
+        //         "234804": 5
+        //     };
+
+        //     const characterCountElement = $("#character-count");
+        //     const messageInput = $("#message");
+        //     const recipientsInput = $("#recipients");
+
+        //     messageInput.on("input", function() {
+        //         const message = $(this).val();
+        //         const messageLength = message.length;
+        //         let remainingChars;
+
+        //         if (messageLength <= pageOneCharLimit) {
+        //             remainingChars = pageOneCharLimit - messageLength;
+        //             characterCountElement.text(`${remainingChars} characters remaining for page 1`);
+        //         } else {
+        //             remainingChars = pageTwoCharLimit - (messageLength - pageOneCharLimit);
+        //             characterCountElement.text(`${remainingChars} characters remaining for page 2`);
+        //         }
+        //     });
+        // });
+
+        // $("#sms-form").submit(function(event) {
+        //     event.preventDefault();
+
+        //     const senderId = $("#sender-id").val();
+        //     let recipients = recipientsInput.val();
+        //     const message = messageInput.val();
+
+        //     // Replace numeric values starting with 0 with 234 in recipients (unchanged)
+        //     recipients = recipients.replace(/\b0(\d+)/g, "234$1");
+
+        //     // Split recipients by comma or new lines
+        //     const recipientsArray = recipients.split(/,|\n/).map(function(recipient) {
+        //         return recipient.trim();
+        //     });
+
+        //     let totalCharge = 0;
+
+        //     recipientsArray.forEach(function(recipient) {
+        //         const prefix = recipient.substr(0, 6);
+        //         const charge = prefixCharges[prefix] || 0;
+        //         totalCharge += charge;
+        //     });
+
+        //     // Display the total charge to the user
+        //     alert(`Total charge: ${totalCharge} unit(s)`);
+
+        //     // Reset the form (unchanged)
+        //     $(this).trigger("reset");
+        // });
         $(document).ready(function() {
+            const prefixCharges = {
+                "234705": 2.2,
+                "234805": 2.2,
+                "234807": 2.2,
+                "234815": 2.2,
+                "234811": 2.2,
+                "234905": 2.2,
+                "234803": 1.9,
+                "234706": 1.9,
+                "234703": 1.9,
+                "234810": 1.9,
+                "234806": 1.9,
+                "234813": 1.9,
+                "234816": 1.9,
+                "234814": 1.9,
+                "234903": 1.9,
+                "234906": 1.9,
+                "234708": 1.9,
+                "234802": 1.9,
+                "234808": 1.9,
+                "234812": 1.9,
+                "234701": 1.9,
+                "234902": 1.9,
+                "234907": 1.9,
+                "234809": 1.9,
+                "234817": 1.9,
+                "234818": 1.9,
+                "234909": 1.9,
+                "234908": 1.9,
+                "234709": 5,
+                "2347027": 5,
+                "2347028": 4,
+                "2347029": 4,
+                "234819": 4,
+                "2347026": 4,
+                "2347025": 4,
+                "234704": 4,
+                "234707": 4,
+                "234804": 5
+            };
+
             const characterCountElement = $("#character-count");
             const messageInput = $("#message");
+            const recipientsInput = $("#recipients");
 
             messageInput.on("input", function() {
                 const message = $(this).val();
                 const messageLength = message.length;
                 let remainingChars;
+                let currentPage = 1;
+                let currentPageLimit = pageOneCharLimit;
 
-                if (messageLength <= pageOneCharLimit) {
-                    remainingChars = pageOneCharLimit - messageLength;
-                    characterCountElement.text(`${remainingChars} characters remaining for page 1`);
-                } else {
-                    remainingChars = pageTwoCharLimit - (messageLength - pageOneCharLimit);
-                    characterCountElement.text(`${remainingChars} characters remaining for page 2`);
+                if (messageLength > pageOneCharLimit) {
+                    const additionalPages = Math.ceil((messageLength - pageOneCharLimit) / pageTwoCharLimit);
+                    currentPage = additionalPages + 1;
+                    currentPageLimit = pageTwoCharLimit;
                 }
+
+                remainingChars = currentPageLimit - (messageLength - (currentPage - 1) * currentPageLimit);
+                characterCountElement.text(`${remainingChars} characters remaining for page ${currentPage}`);
             });
+
 
             $("#sms-form").submit(function(event) {
                 event.preventDefault();
@@ -113,7 +250,7 @@
                 let recipients = recipientsInput.val();
                 const message = messageInput.val();
 
-                // Replace numeric values starting with 0 with 234 in recipients
+                // Replace numeric values starting with 0 with 234 in recipients (unchanged)
                 recipients = recipients.replace(/\b0(\d+)/g, "234$1");
 
                 // Split recipients by comma or new lines
@@ -121,14 +258,35 @@
                     return recipient.trim();
                 });
 
-                // Perform SMS sending logic here
-                // You can use AJAX or fetch to send the data to the server
+                let totalPages = 0;
+                let totalCharge = 0;
 
-                // Reset the form
+                recipientsArray.forEach(function(recipient) {
+                    const prefix = recipient.substr(0, 6);
+                    const charge = prefixCharges[prefix] || 0;
+
+                    // Calculate the number of pages based on the message length
+                    const messageLength = message.length;
+                    const numPages = Math.ceil(messageLength / 160);
+
+                    // Calculate the charge per page for the current recipient
+                    const recipientCharge = charge * numPages;
+
+                    // Add the recipient's charge and number of pages to the total
+                    totalCharge += recipientCharge;
+                    totalPages += numPages;
+                });
+
+                // Display the number of pages and total charge to the user
+                alert(`You are about to send to ${recipientsArray.length} recipient(s). Total charge: ${totalCharge.toFixed(2)} unit(s)`);
+                // alert(`You are about to send ${totalPages} page(s) to ${recipientsArray.length} recipient(s). Total charge: ${totalCharge.toFixed(2)} unit(s)`);
+
+                // Reset the form (unchanged)
                 $(this).trigger("reset");
             });
         });
     </script>
+
 
 
 </body>
